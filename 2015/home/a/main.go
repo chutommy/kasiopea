f42c2db32bcc6f5a957c11c9e884b21ff013bddc
Tommy Chu <tommychu2256@gmail.com>
2020-09-18 20:36:11 +0200

    Added solution for 2015/home/a

---
+++ 2015/home/a/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	// create output file
+	f, err := os.Create("a.out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// get T
+	var T int
+	fmt.Scan(&T)
+
+	for t := 0; t < T; t++ {
+
+		// get N
+		var N int
+		fmt.Scan(&N)
+
+		// get input
+		inp := make([]int, N)
+		for n := 0; n < N; n++ {
+
+			// get int
+			var i int
+			fmt.Scan(&i)
+			// store
+			inp[n] = i
+		}
+
+		// solve
+		s := solve(inp, N)
+		fmt.Fprintln(f, s)
+	}
+}
+
+func solve(inp []int, N int) int {
+
+	count := 0
+	dest := N - 1
+
+	for n := 0; n < dest; {
+
+		n += inp[n]
+		count++
+	}
+
+	return count
+}
