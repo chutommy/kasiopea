47cb69e85989b8f6ec3906c775242a5977e700ea
Tommy Chu <tommychu2256@gmail.com>
2020-09-21 17:03:34 +0200

    Added solution for 2015/home/h (need validation)

---
+++ 2015/home/h/go.mod
@@ -0,0 +1,3 @@
+module github.com/chutified/kasiopea/2015/home/h
+
+go 1.15
---
+++ 2015/home/h/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	// create output file
+	f, err := os.Create("h.out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// get T
+	var T int
+	fmt.Scan(&T)
+
+	for t := 0; t < T; t++ {
+
+		// get M, N
+		var M, N int
+		fmt.Scan(&M, &N)
+
+		// get input
+		inp := make([]int, N)
+		for n := 0; n < N; n++ {
+			var i int
+			fmt.Scan(&i)
+			inp[n] = i
+		}
+
+		// solve
+		sol := solve(inp, M, N)
+		fmt.Fprintln(f, sol)
+	}
+}
+
+func solve(inp []int, M, N int) int {
+
+	q := inp
+
+	for {
+		val := q[0]
+
+		if val%M == 0 {
+			return val
+		}
+
+		for _, v := range inp {
+			q = append(q, val*10+v)
+		}
+
+		q = q[1:]
+	}
+}
