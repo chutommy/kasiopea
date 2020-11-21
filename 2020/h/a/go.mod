421b9220c2f22e7b5a77c623fac0d11c323a41bd
Tommy Chu <tommychu2256@gmail.com>
2020-11-21 18:40:36 +0100

    Added solution for 2020/h/a

---
+++ 2020/h/a/go.mod
@@ -0,0 +1,3 @@
+module github.com/chutified/kasiopea/2020/h/a
+
+go 1.15
---
+++ 2020/h/a/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	// create an output file
+	f, err := os.Create("a.out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// get T
+	var T int
+	fmt.Scan(&T)
+
+	// range over each problem
+	for t := 0; t < T; t++ {
+
+		// get input
+		var W, WO, P int
+		fmt.Scanf("%d %d %d", &W, &WO, &P)
+
+		// solve
+		if WO < W-P {
+			fmt.Fprintln(f, "REKLAMU")
+		} else {
+			fmt.Fprintln(f, "NE REKLAMU")
+		}
+	}
+}
