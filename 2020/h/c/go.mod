dd0faf1c5a728aad63597215ac17cf382afe364e
Tommy Chu <tommychu2256@gmail.com>
2020-11-21 18:44:10 +0100

    Added solution for 2020/h/c

---
+++ 2020/h/c/go.mod
@@ -0,0 +1,3 @@
+module github.com/chutified/kasiopea/2020/h/c
+
+go 1.15
---
+++ 2020/h/c/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+)
+
+func main() {
+
+	// create output file
+	f, err := os.Create("c.out")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// get T
+	var T int
+	fmt.Scan(&T)
+
+	// range over problems
+	for t := 0; t < T; t++ {
+
+		// get N
+		var N int
+		fmt.Scan(&N)
+
+		// get distances
+		dists := make([]int, N)
+		for n := 0; n < N; n++ {
+
+			// get distances
+			var d int
+			fmt.Scan(&d)
+			dists[n] = d
+		}
+
+		// solve
+		s := solve(N, dists)
+		fmt.Fprintln(f, s)
+	}
+}
+
+func solve(N int, dists []int) float64 {
+
+	// sort
+	sort.Ints(dists)
+
+	// get maximal dist diff
+	var max int
+	for i := 1; i < N; i++ {
+		if v := dists[i] - dists[i-1]; v > max {
+			max = v
+		}
+	}
+
+	return float64(max) / 2
+}
